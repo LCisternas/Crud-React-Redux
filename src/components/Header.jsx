4b8f87f57f9fe7e19f7c549a748f08b66362b457
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-08-19 12:23:51 -0400

    detail

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header-container'>
       <h1>Crud React-Redux</h1>
-      <a href='!#'><i className="fab fa-github"></i></a>
+      <a href='https://github.com/LCisternas/Crud-React-Redux' target='_blank' rel="noreferrer" ><i className="fab fa-github"></i></a>
     </div>
   );
 }
